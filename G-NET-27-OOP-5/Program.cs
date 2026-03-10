3002f528f0358e66ee75a1073df8981a30295f58
MostafaRamadan27 <mostafaramadan200018@gmail.com>
2026-03-10 02:56:40 +0200

    Q!

--- G-NET-27-OOP-5/Program.cs
+++ G-NET-27-OOP-5/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 : Theoretical Questions
+
+            #region Q1
+              // interface is Contract that class must follow , it specifies what class can do no what how does it , because tight coupling and hard to test , hard to change
+              // 1-enable multiable inheritance
+              // 2- enable polymorphism without inheritance
+              // 3- remove tight coupling between classes  
+
+            #endregion
+
+            #endregion
         }
     }
 }
