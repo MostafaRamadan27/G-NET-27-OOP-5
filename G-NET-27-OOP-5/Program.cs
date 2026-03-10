e154e7eff6609e9bda276a8f49741b4af1a0f270
MostafaRamadan27 <mostafaramadan200018@gmail.com>
2026-03-10 03:15:27 +0200

    Q2

--- G-NET-27-OOP-5/Program.cs
+++ G-NET-27-OOP-5/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace G_NET_27_OOP_5
+﻿using System.Runtime.InteropServices;
+
+namespace G_NET_27_OOP_5
 {
     internal class Program
     {
@@ -7,13 +9,24 @@
             #region Part 01 : Theoretical Questions
 
             #region Q1
-              // interface is Contract that class must follow , it specifies what class can do no what how does it , because tight coupling and hard to test , hard to change
-              // 1-enable multiable inheritance
-              // 2- enable polymorphism without inheritance
-              // 3- remove tight coupling between classes  
+            // interface is Contract that class must follow , it specifies what class can do no what how does it , because tight coupling and hard to test , hard to change
+            // 1-enable multiable inheritance
+            // 2- enable polymorphism without inheritance
+            // 3- remove tight coupling between classes  
 
             #endregion
 
+            #region Q2
+            // A) IEnglishSpeaker and IArabicSpeaker exact same thing we cant give them different brhavior 
+            // print same result in both cases
+            // B) Explicit implementation 
+            //C) yes can call Translator.Greet() we call each version by Cast interface first 
+            //like Translator translator = new Translator();
+            //IEnglishSpeaker englishSpeaker = translator;
+            // englishSpeaker.Greet(); // Calls IEnglishSpeaker's Greet method
+            // IArabicSpeaker arabicSpeaker = translator;
+            // arabicSpeaker.Greet(); // Calls IArabicSpeaker's Greet method
+            #endregion
             #endregion
         }
     }
